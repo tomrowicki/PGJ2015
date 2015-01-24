f33e94bc845e3bac40f8dc7687da47db5755fcbe
mrUwa777 <tomrowicki@gmail.com>
2015-01-24 14:17:03 +0100

    Kontrolery

--- src/pl/pgj2015/controller/ControllerConfig.java
+++ src/pl/pgj2015/controller/ControllerConfig.java
@@ -2,16 +2,43 @@ package pl.pgj2015.controller;
 
 import java.util.Map;
 
-public class ControllerConfig  {
+public class ControllerConfig {
 
-	private Map<PlayerNumber, Map<Integer, ActionKey>> config;  
-	
-	public Map<Integer, ActionKey> getConfigForPlayer(PlayerNumber player){
-	
+	private Map<PlayerNumber, Map<Integer, ActionKey>> config;
+	private Map<Integer, ActionKey> keysMapping; 
+
+	public Map<Integer, ActionKey> getConfigForPlayer(PlayerNumber player) {
+
 		return config.get(player);
 	}
-	
-	public void setConfigurationForPlayer(PlayerNumber player){
 
+	public void setConfigurationForPlayer(PlayerNumber player) {
+
+		if (player == PlayerNumber.PLAYER_ONE) {
+			
+			keysMapping.put((int) 'w', ActionKey.UP);
+			keysMapping.put((int) 's', ActionKey.DOWN);
+			keysMapping.put((int) 'a', ActionKey.LEFT);
+			keysMapping.put((int) 'd', ActionKey.RIGHT);
+			keysMapping.put((int) 'v', ActionKey.SHOOT);
 		}
-	}	
+		else if (player == PlayerNumber.PLAYER_TWO){
+			
+			/*left arrow: 37 
+			up arrow: 38
+			right arrow: 39
+			down arrow: 40
+			 */
+			keysMapping.put(38, ActionKey.UP);
+			keysMapping.put(40, ActionKey.DOWN);
+			keysMapping.put(37, ActionKey.LEFT);
+			keysMapping.put(39, ActionKey.RIGHT);
+			keysMapping.put((int) 'p', ActionKey.SHOOT);
+		}
+
+	}
+
+	public Map<Integer, ActionKey> getKeysMapping() {
+		return keysMapping;
+	}
+}
--- src/pl/pgj2015/controller/KeyboardController.java
+++ src/pl/pgj2015/controller/KeyboardController.java
@@ -1,33 +1,49 @@
 package pl.pgj2015.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class KeyboardController implements Controller {
 
 	private PlayerNumber player;
+	private Map<Integer, ActionKey> keysMapping;
+	private Map<ActionKey, Boolean> keysPressed;
 	
 	public KeyboardController (PlayerNumber player){
 		
 		this.player = player;
+		ControllerConfig config = new ControllerConfig();
+		config.setConfigurationForPlayer(player);
+		keysMapping = config.getKeysMapping();
 	}
 
 	@Override
 	public void keyPressed(int key) {
-		// TODO Auto-generated method stub
-		
+
+		if(keysMapping.containsKey(key)){
+			keysPressed.put(keysMapping.get(key), true);
+		}
 	}
 
 	@Override
 	public void keyReleased(int key) {
-		// TODO Auto-generated method stub
-		
+
+		if(keysMapping.containsKey(key)){
+			keysPressed.put(keysMapping.get(key), false);
+		}
 	}
 
 	@Override
 	public List<ActionKey> getKeysPressed() {
-		// TODO Auto-generated method stub
-		return null;
+
+		List<ActionKey> keysPressedList = new ArrayList<>();
+		for(ActionKey actionKey : ActionKey.values()){
+			if(keysPressed.get(actionKey)){
+				keysPressedList.add(actionKey);
+			}
+		}
+		return keysPressedList;
 	}
 
 	@Override
